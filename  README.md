b7c25d5eb9583fcdfa65487796b2cf623effd12a
think2011 <452125301@qq.com>
2016-01-08 13:51:21 +0800

    initial commit

---
+++  README.md
@@ -0,0 +1,9 @@
+## ng-air-datepicker
+对air-datepicker的angula封装
+
+## 例子
+
+```html
+<input date-picker type="text" options="{language: 'en'}" ng-model="xxx">
+<input date-picker type="text" options="xxx" ng-model="xxxx">
+```
---
+++ ng-air-datePicker.js
@@ -0,0 +1,56 @@
+(function () {
+    // Example from https://github.com/t1m0n/air-datepicker
+
+    angular
+        .module('air-datePicker', [])
+        .directive('datePicker', datePicker);
+
+    /**
+     * air-datePicker
+     *
+     * @scope ngModel {object|ngModel} object or scope
+     * @scope options {object} air-datePicker options
+     *
+     * @example
+     * <input date-picker type="text" options="{language: 'en'}" ng-model="xxx">
+     * <input date-picker type="text" options="xxx" ng-model="xxxx">
+     */
+    function datePicker ($rootScope, $timeout) {
+        return {
+            restrict: 'A',
+            scope   : {
+                options: '&',
+                ngModel: '='
+            },
+            link    : linkFunc
+        };
+
+        function linkFunc (scope, element, attrs, ctrl) {
+            var defaults = {
+                language: 'zh'
+            };
+
+            // view to model
+            defaults.onSelect = function (formattedDate, date, inst) {
+                scope.ngModel = formattedDate;
+                scope.$applyAsync();
+            };
+
+            // model to view
+            scope.$watch('ngModel', function (newVal) {
+                if (!newVal) return false;
+
+                $(element).data('datepicker').selectDate(new Date(newVal));
+            });
+
+            // init
+            scope.$watch(function () {
+                return scope.options();
+            }, function (newVal) {
+                $(element).datepicker(angular.extend(defaults, newVal));
+            }, true);
+
+            $(element).datepicker(angular.extend(defaults, scope.options()));
+        }
+    }
+})();
